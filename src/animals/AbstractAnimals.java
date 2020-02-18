f06d4a2fbf2e9999d539581ac5ee7c4979b6ffab
MrGio7 <lashagiorgi1233@gmail.com>
2020-02-18 19:10:47 +0400

    created abstract animal class

---
+++ src/animals/AbstractAnimals.java
@@ -0,0 +1,53 @@
+package animals;
+
+public abstract class AbstractAnimals {
+    protected static int maxId = 0;
+    protected int id;
+    protected String name;
+    protected int yearNamed;
+    protected int food;
+    protected String implement;
+
+    public AbstractAnimals(String name, int yearNamed, int food, String implement){
+        maxId++;
+        id = maxId;
+
+        this.name = name;
+        this.yearNamed = yearNamed;
+        this.food = food;
+        this.implement = implement;
+    }
+
+    public abstract String getMove();
+    public abstract String getBreath();
+    public abstract String getReproduce();
+
+    public void setName(String name) {
+        this.name = name;
+    } 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setYear(int yearNamed) {
+        this.yearNamed = yearNamed;
+    }
+
+    public int getYear(){
+        return yearNamed;
+    }
+
+    public void setFood(int food) {
+        this.food = food;
+    }
+
+    public void eatFood(int i) {
+        this.food = food - i;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+}
